a2a4c40e02dfa5f127301a6da1a2e5e78fa1ff46
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-04-25 08:59:13 -0600

    snippets controllers routes created

---
+++ routes/api/snippets.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const snippetsController = require('../../controllers/api/snippets');
+
+router.post('/create', snippetsController.create);
+router.put('/update/:id', snippetsController.update);
+router.delete('/delete/:id', snippetsController.delete);
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(require("./config/checkToken"));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/categories", require("./routes/api/categories"));
+app.use("/api/snippets", require("./routes/api/snippets"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));

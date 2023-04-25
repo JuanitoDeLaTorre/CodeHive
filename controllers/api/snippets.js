bd4401e68c0a31da7f18e64adb23d50df15ff462
ahmadkismael <ahmad.abualkhair@gmail.com>
2023-04-25 08:19:21 -0600

    controllers snippets update function

--- controllers/api/snippets.js
+++ controllers/api/snippets.js
@@ -17,6 +17,17 @@ async function create(req, res) {
   }
 }
 
+
+async function update(req, res) {
+    try {
+    const snippet = await Snippet.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(snippet);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
+}
+
 moduel.exports = {
   create,
   update,
